010739c87931f5e1ff805d4a738d0e8fa91e36e2
marc vargas <marcvargas@marcs-Air.attlocal.net>
2023-07-26 12:46:50 -0500

    added sample questions

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const inquirer = require('inquirer')
 
-console.log("hello world")
+inquirer.prompt([
+    {
+        type: 'input', 
+        message: 'What is your favorite hamburger place?',
+        name: 'fav_hamburger' // fav = favorite
+    },
+    {
+        type: 'input', 
+        message: 'What is your favorite soda?',
+        name: 'soda'
+    }
+]).then((answers) => {
+    console.log(answers)
+})
