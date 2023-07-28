248f4af9797dd3502c5602933ffe0280e7ffe3e1
marc vargas <marcvargas@marcs-Air.attlocal.net>
2023-07-28 13:31:19 -0500

    added questions

--- index.js
+++ index.js
@@ -3,13 +3,58 @@ const inquirer = require('inquirer')
 inquirer.prompt([
     {
         type: 'input', 
-        message: 'What is your favorite hamburger place?',
-        name: 'fav_hamburger' // fav = favorite
+        message: 'What is your project title?',
+        name: 'title' 
     },
     {
         type: 'input', 
-        message: 'What is your favorite soda?',
-        name: 'soda'
+        message: 'What does your project do?',
+        name: 'description'
+    },
+    {
+        type: 'input', 
+        message: 'What is your GitHub username?',
+        name: 'username'
+    },
+    {
+        type: 'input', 
+        message: 'What is your email address?',
+        name: 'email'
+    },
+    {
+        type: 'list', 
+        message: 'What license for your application will be used?',
+        name: 'license',
+        choices: [
+            'MIT',
+            'ISC',
+            'ABC'
+        ]
+    },
+    {
+        type: 'input', 
+        message: 'How is your project installed?',
+        name: 'installion'
+    },
+    {
+        type: 'input', 
+        message: 'How do you start your application?',
+        name: 'usage'
+    },
+    {
+        type: 'input', 
+        message: 'How do you start your application?',
+        name: 'usage'
+    },
+    {
+        type: 'input', 
+        message: 'What are your contribion guidelines?',
+        name: 'guidelines'
+    },
+    {
+        type: 'input', 
+        message: 'what are your test instructions?',
+        name: 'instructions'
     }
 ]).then((answers) => {
     console.log(answers)
